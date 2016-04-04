663984e6d2d657035e6c7a531d126685946d5e9a
nabil hassein <nabil.hassein@gmail.com>
2016-04-04 17:17:43 -0400

    add derived column to each row: max violation count across years and precincts

--- src/clean.py
+++ src/clean.py
@@ -2,11 +2,20 @@ import pandas as pd
 
 xls = pd.ExcelFile('data/c_summons_cw_2007_ytd_2015.xlsx')
 df = xls.parse('Sheet1', skiprows=4, skip_footer = 19)
-
 df = df.fillna(method='ffill')
 
 df = df[~df.Precinct.str.contains('CITYWIDE')]
 df = df[~df.Precinct.str.contains('Total')]
 df = df[~df.Violation.str.contains('Total')]
 
-df.to_csv('data/clean-summons-data.csv')
+violations = pd.Series.unique(df.Violation)
+violationMax = {}
+
+for violation in violations:
+    subset = df[df.Violation == violation]
+    subset = subset.drop(subset.columns[[0, 1, 2]], axis = 1)
+    violationMax[violation] = max(subset.max(numericOnly = True))
+
+df['Max'] = df.apply(lambda row: violationMax[row.Violation], axis = 1)
+
+df.to_csv('data/clean-summons-data.csv', index_label="Id")
