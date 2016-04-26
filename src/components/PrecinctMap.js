9f88eb5579384d7f435c7baef5d67afdf7a622a0
nabil hassein <nabil.hassein@gmail.com>
2016-04-26 18:10:37 -0400

    add a tooltip on hover over a precinct

--- src/components/PrecinctMap.js
+++ src/components/PrecinctMap.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import d3 from 'd3'
 
-export default class PrecinctMap extends React.Component {
+const mapStateToProps = state => {
+    return {
+        currentPrecinct: state.currentPrecinct,
+        mouseX: state.mouseX,
+        mouseY: state.mouseY,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        mouseover: precinct => {
+            dispatch({
+                type: "PRECINCT_HOVER",
+                currentPrecinct: precinct,
+                mouseX: d3.event.clientX,
+                mouseY: d3.event.clientY,
+            })
+        },
+        mouseout: () => {
+            dispatch({
+                type: "PRECINCT_UNHOVER",
+            })
+        },
+    }
+}
+
+
+class PrecinctMap extends React.Component {
     constructor() {
         super();
         this.id = "PrecinctMap";
         this.precinctClass = "precinct";
+        this.tooltipRectClass = "d3-tooltip-rect";
+        this.tooltipTextClass = "d3-tooltip-text";
         this.bucketToColor = {
             q0: "rgb(247,251,255)",
             q1: "rgb(222,235,247)",
@@ -28,7 +58,9 @@ export default class PrecinctMap extends React.Component {
               height = window.innerHeight * .8,
               id = this.id,
               precinctClass = this.precinctClass,
-              features = this.props.precinctJson.features;
+              features = this.props.precinctJson.features,
+              mouseover = this.props.mouseover,
+              mouseout = this.props.mouseout;
 
         this.svg = d3.select("#" + id).append("svg")
             .attr("width", width)
@@ -47,6 +79,8 @@ export default class PrecinctMap extends React.Component {
             .data(features)
             .enter().append("path")
             .attr("d", path)
+            .on("mouseover", e => mouseover(e.properties.Precinct))
+            .on("mouseout", e => mouseout())
             .classed(precinctClass, true);
 
         this.componentWillReceiveProps(this.props);
@@ -67,5 +101,60 @@ export default class PrecinctMap extends React.Component {
 
                 return bucketToColor[bucket];
             });
+
+        this.drawTooltip(newProps.currentPrecinct, newProps.violationData, newProps.mouseX, newProps.mouseY);
+    }
+
+    drawTooltip(selectedPrecinct, violationData, mouseX, mouseY) {
+        if (selectedPrecinct) {
+            const tooltipRectClass = this.tooltipRectClass,
+                  tooltipTextClass = this.tooltipTextClass,
+                  xOffset = 42,
+                  yOffset = 25;
+
+            const g = this.svg.append("g").attr('x', mouseX).attr('y', mouseY);
+
+            const tooltipText = g.selectAll("." + tooltipTextClass)
+                  .data([selectedPrecinct]);
+
+            const text1 = tooltipText.enter().append('text')
+                .attr('class', tooltipTextClass)
+                .attr('x', mouseX + xOffset)
+                .attr('y', mouseY + yOffset)
+                .attr('dy', '0.35em')
+                .attr('text-anchor', 'middle')
+                .text("Precinct: " +  selectedPrecinct);
+
+            const text2 = tooltipText.enter().append('text')
+                .attr('class', tooltipTextClass)
+                .attr('x', mouseX + xOffset)
+                .attr('y', mouseY + yOffset * 2)
+                .attr('dy', '0.35em')
+                .attr('text-anchor', 'middle')
+                .text("Violations: " + (violationData[selectedPrecinct] || 0));
+
+            tooltipText.exit().remove();
+
+            const bbox1 = text1.node().getBBox();
+            const bbox2 = text2.node().getBBox();
+
+            const tooltipRect = g.selectAll("." + tooltipRectClass)
+                  .data([selectedPrecinct]);
+
+            tooltipRect.enter().append("rect")
+                .attr("class", tooltipRectClass)
+                .attr("height", bbox2.height * 3)
+                .attr("width", bbox2.width)
+                .attr('x', bbox2.x)
+                .attr('y', bbox1.y)
+                .style("fill", "yellow")
+                .style("fill-opacity", ".3");
+
+            tooltipRect.exit().remove();
+        } else {
+            d3.select("#" + this.id).select("svg").selectAll("g").remove();
+        }
     }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(PrecinctMap);
--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,7 @@
 const defaultState = {
     "currentYear": "2007",
     "currentViolation": "DISORDERLY CONDUCT",
+    "currentPrecinct": null,
 }
 
 export default function reducer(state = defaultState, action) {
@@ -9,6 +10,10 @@ export default function reducer(state = defaultState, action) {
         return {...state, currentYear: action.currentYear};
     case 'UPDATE_VIOLATION':
         return {...state, currentViolation: action.currentViolation};
+    case 'PRECINCT_HOVER':
+        return {...state, currentPrecinct: action.currentPrecinct, mouseX: action.mouseX, mouseY: action.mouseY };
+    case 'PRECINCT_UNHOVER':
+        return {...state, currentPrecinct: null, mouseX: null, mouseY: null };
     default:
         return state;
     }
